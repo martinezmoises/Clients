4a4ac7b96020669c0bde19362ad8a3c12f0995cb
Moises Martinez <2021154670@ub.edu.bz>
2023-03-01 13:10:37 -0600

    Code with comments

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	//to where we are making the request(client)
-	url := "https://api.github.com"
+	url := "https://api.github.com" //API github.com (Server)
 
 	//creat a request(new request)
 	request, err := http.NewRequest(http.MethodGet, url, nil)
@@ -18,18 +18,21 @@ func main() {
 	}
 
 	//creating a client
-	client := http.Client{}
-	response, err := client.Do(request)
+	client := http.Client{} //This does not know how long it would wait for response
+	//Generic client        //Type called type creating an instances of this type called client
+	response, err := client.Do(request) //This is sending request (API GITHUB.COM)
 	if err != nil {
-		panic(err)
+		panic(err) //Program stops
 	}
 	//we need to close the response
 	defer response.Body.Close()
 
-	bytes, err := io.ReadAll(response.Body)
+	bytes, err := io.ReadAll(response.Body) //Returns bytes
 	if err != nil {
 		panic(err)
 	}
+
+	//IF this codes executes we did not get an error (Response is valid)
 	fmt.Println(response.StatusCode)
-	fmt.Println(string(bytes))
+	fmt.Println(string(bytes)) //Converts bytes so it can be readable
 }

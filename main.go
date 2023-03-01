516569087b40b42f38f35d53cd26c0cd22d947ea
Moises Martinez <2021154670@ub.edu.bz>
2023-03-01 12:52:10 -0600

    This the client code

--- main.go
+++ main.go
@@ -11,9 +11,15 @@ func main() {
 	//to where we are making the request(client)
 	url := "https://api.github.com"
 
+	//creat a request(new request)
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		panic(err)
+	}
+
 	//creating a client
 	client := http.Client{}
-	response, err := client.Get(url)
+	response, err := client.Do(request)
 	if err != nil {
 		panic(err)
 	}
